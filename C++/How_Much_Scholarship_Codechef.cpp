ee3273dc47dfacfbc6e3c34ccf407274249e6220
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-04-16 00:27:50 +0530

    Create How_Much_Scholarship_Codechef.cpp

---
+++ C++/How_Much_Scholarship_Codechef.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int r;
+	cin>>r;
+	if(r>0 && r<=50)
+	{
+	    cout<<100<<endl;
+	}
+	else if(r>=51 && r<=100)
+	{
+	    cout<<50<<endl;
+	}
+	else
+	{
+	    cout<<0<<endl;
+	}
+	return 0;
+}
