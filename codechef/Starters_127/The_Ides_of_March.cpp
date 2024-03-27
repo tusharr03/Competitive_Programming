a42d29ad9f72263e0eda2e3b84b388cd81e65109
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2024-03-27 21:25:12 +0530

    Create The_Ides_of_March.cpp

---
+++ codechef/Starters_127/The_Ides_of_March.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n==15)
+	{
+	    cout<<"Yes";
+	}
+    else
+    {
+        cout<<"No";
+    }
+    return 0;
+}
